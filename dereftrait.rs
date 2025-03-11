e8427f3ba9e697f982740a7d2f3c2b5b08503313
makmot256 <makmot.kabirajohnson@students.mak.ac.ug>
2025-03-11 14:29:07 +0300

    deref traits

---
+++ dereftrait.rs
@@ -0,0 +1,28 @@
+use std::ops::Deref;
+
+// Define a struct that will implement the Deref trait
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
+fn main() {
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    // Deref coercion allows us to call methods on the inner type directly
+    assert_eq!(5, *y);
+}
+
