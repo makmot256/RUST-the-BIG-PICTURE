4d3226b46ffc871fd760606968896c93e9f0f3f1
makmot256 <makmot.kabirajohnson@students.mak.ac.ug>
2025-03-15 08:56:13 +0300

    unwrap rust file

---
+++ unwrap.rs
@@ -0,0 +1,317 @@
+fn main() {
+    let some_number = Some(5);
+    let the_number = some_number.unwrap();
+    println!("The number is {}", the_number);
+}
+
+fn main() {
+
+    let some_number = Some(5);
+    match some_number {
+        Some(n) => println!("The number is {}", n),
+        None => println!("There is no number"),
+    }
+
+    let some_number = Some(5);
+    let the_number = some_number.expect("There is no number");
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or(0);
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or(0);
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| 0);
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| 0);
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| panic!("There is no number"));
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| panic!("There is no number"));
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = Some(5);
+    let the_number = some_number.unwrap_or_else(|_| {
+        println!("There is no number");
+        0
+    });
+    println!("The number is {}", the_number);
+
+    let some_number = None;
+
